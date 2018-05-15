06b44898a2b0b7e254ba4ee65909144457bf54df
Levi Rosenberg <levirosenberg97@gmail.com>
2018-05-14 17:52:26 -0700

    added a player control and shooting script

---
+++ Assets/Levi/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XInputDotNetPure;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+
+    PlayerIndex playerIndex;
+    GamePadState state;
+    GamePadState prevState;
+
+    float moveVertical;
+    float moveHorizontal;
+    Vector3 movement;
+
+
+    void Move()
+    {
+      if(moveHorizontal!= 0|| moveVertical != 0)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation,
+                                                  Quaternion.LookRotation(movement),
+                                                  9 * Time.deltaTime);
+        }
+
+        moveHorizontal = state.ThumbSticks.Left.X;
+        moveVertical = state.ThumbSticks.Left.Y;
+
+        movement = new Vector3(moveHorizontal, 0f, moveVertical);
+
+        transform.Translate(movement * speed * Time.deltaTime, Space.World);
+    }
+    
+	void Update ()
+    {
+        prevState = state;
+        state = GamePad.GetState(playerIndex);
+
+        Move();
+	}
+}
