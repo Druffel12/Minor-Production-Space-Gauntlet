124ec498e4f1feb61b4d8f35c36787bb8c28f4fd
Cole Druffel <Druffel12@gmail.com>
2018-05-14 17:51:23 -0700

    Work done on AI side note levi likes to call people dad

---
+++ Assets/Cole/Scripts/AIMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIMovement : MonoBehaviour
+{
+    public float Speed;
+    private Transform Player;
+    Vector3 Dir;
+    
+    private void Awake()
+    {
+        MyTransform = transform;
+    }
+    // Use this for initialization
+    void Start ()
+    {
+        if (Player == null)
+        {
+            Player = GameObject.FindGameObjectWithTag("Player").transform;
+        }
+
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        // Vector3 lookat = Player.position;
+        //transform.LookAt(lookat);
+
+        Dir = MyTransform.position - Player.transform.position;
+        MyTransform.position -= Dir.normalized * Speed; 
+		
+	}
+}
