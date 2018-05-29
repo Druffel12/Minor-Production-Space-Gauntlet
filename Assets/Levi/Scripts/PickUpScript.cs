c9ad80aa61c00b82ab30850165981dd3e615427e
Levi Rosenberg <levirosenberg97@gmail.com>
2018-05-29 15:06:30 -0700

    Finished the Score Counter

--- Assets/Levi/Scripts/PickUpScript.cs
+++ Assets/Levi/Scripts/PickUpScript.cs
@@ -1,27 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using XInputDotNetPure;
 
 public class PickUpScript : MonoBehaviour
 {
-    public float healthIncrease;
+    float healthIncrease;
+    int score;
+    public PickUpStatsObj stats;
+
+    private void Start()
+    {
+        healthIncrease = stats.healthIncrease;
+        score = stats.scoreIncrease;
+    }
 
     private void OnTriggerEnter(Collider other)
     {
      if(other.gameObject.layer == LayerMask.NameToLayer("Player"))
         {
-            HP playerHP = other.GetComponent<HP>();
-
-            if(playerHP != null)
+            if (gameObject.tag == "HealthPickUp")
             {
-                playerHP.health += healthIncrease;
-                playerHP.healthText.text = playerHP.health.ToString();
+                HP playerHP = other.GetComponent<HP>();
 
-                //if (playerHP.health >= playerHP.maxHP)
-                //{
-                //    playerHP.health = playerHP.maxHP;
-                //}
-                Destroy(gameObject);
+                if (playerHP != null)
+                {
+                    playerHP.health += healthIncrease;
+                    playerHP.healthText.text = playerHP.health.ToString();
+                    Destroy(gameObject);
+                }
+            }
+
+            if(gameObject.tag == "Treasure")
+            {
+                ScoreCounter playerScore = other.GetComponent<ScoreCounter>();
+                if (playerScore != null)
+                {
+                    playerScore.ScoreIncrease(score);
+                    Destroy(gameObject);
+                }
             }
         }
     }
