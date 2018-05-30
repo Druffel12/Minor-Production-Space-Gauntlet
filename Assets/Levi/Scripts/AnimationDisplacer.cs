6eb3b8644805b5c6f8625988487642e448074fc4
Levi Rosenberg <levirosenberg97@gmail.com>
2018-05-30 10:04:25 -0700

    Started work on an animation displacer

---
+++ Assets/Levi/Scripts/AnimationDisplacer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationDisplacer : MonoBehaviour
+{
+    Animation anim;
+    public string animationName;
+    public string root;
+
+	void Start ()
+    {
+        
+	}
+	
+	public void ChangeAnimation()
+    {
+        Transform mixTransform = transform.Find(root);
+
+        anim[animationName].AddMixingTransform(mixTransform);
+    }
+	
+}
