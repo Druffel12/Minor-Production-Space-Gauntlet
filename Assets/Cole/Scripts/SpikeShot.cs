07921cc619668eda7a038e68715408efe4aebd8e
Cole Druffel <Druffel12@gmail.com>
2018-06-20 12:43:09 -0700

    after lunch merge

---
+++ Assets/Cole/Scripts/SpikeShot.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeShot : MonoBehaviour {
+
+    public float SpikeDamage;
+    public float SpikeLife;
+    public ObjectPooler MyPool;
+    public EnemySpawner Spawner;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        HP Temp = other.GetComponent<HP>();
+        if(Temp != null)
+        {
+            Temp.Damage(SpikeDamage);
+        }
+   
+    }
+
+    private void Start()
+    {
+        MyPool = ObjectPooler.SharedInstance;
+    }
+
+    private void Update()
+    {
+        if (SpikeLife <= 0.0f)
+        {
+            ReturnToPool();
+        }
+    }
+
+    void ReturnToPool()
+    {
+        MyPool.PooledObjects.Add(gameObject);
+        gameObject.SetActive(false);
+        if(Spawner != null)
+        {
+            Spawner.SpawnCount++;
+        }
+    }
+}
